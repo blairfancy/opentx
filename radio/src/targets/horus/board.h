cbf9d130481cbde11d8d19fd381cce9e2eac40aa
blairfancy <blairfancy@hotmail.com>
2020-02-16 17:34:28 +0100

    Merge with latest opentx official

--- radio/src/targets/horus/board.h
+++ radio/src/targets/horus/board.h
@@ -553,8 +553,8 @@ void usbJoystickUpdate();
   #define USB_PRODUCT                  'T', '1', '6', ' ', ' ', ' ', ' ', ' '  /* 8 Bytes */  
 #elif defined(RADIO_TX16S)
   #define USB_NAME                     "RadioMas TX16S"
-  #define USB_MANUFACTURER             'R', 'a', 'd', 'i', 'o', 'M', 'a ', 's '  /* 8 bytes */
-  #define USB_PRODUCT                  'T', 'X', '1', '6 ', 'S ', ' ', ' ', ' '  /* 8 Bytes */
+  #define USB_MANUFACTURER             'R', 'a', 'd', 'i', 'o', 'M', 'a', 's'  /* 8 bytes */
+  #define USB_PRODUCT                  'T', 'X', '1', '6', 'S', ' ', ' ', ' '  /* 8 Bytes */
 #elif defined(PCBX10)
   #define USB_NAME                     "FrSky X10"
   #define USB_MANUFACTURER             'F', 'r', 'S', 'k', 'y', ' ', ' ', ' '  /* 8 bytes */
@@ -596,7 +596,7 @@ extern uint32_t telemetryErrors;
 void telemetryPortInvertedInit(uint32_t baudrate);
 
 // Sport update driver
-#if defined(PCBX10) && !defined(RADIO_T16)
+#if HAS_SPORT_UPDATE_CONNECTOR()
 void sportUpdatePowerOn();
 void sportUpdatePowerOff();
 #define SPORT_UPDATE_POWER_ON()        sportUpdatePowerOn()
